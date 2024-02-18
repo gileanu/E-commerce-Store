acf5ee5db39391a3c5d4fbe4c9afc54a8458f40c
gileanu <gileanu@gmail.com>
2024-02-18 17:05:45 +0200

    [main] connected api, added navbar

---
+++ components/footer.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <footer className="bg-white border-t">
+      <div className="mx-auto py-10">
+        <p className="text-center test-xs text-black">
+          &copy; 2024 Test Store, All rights reserved.
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
