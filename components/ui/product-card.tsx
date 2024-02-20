9db7a4f169c12c995be4aa6e6b74d1f78a0eeb52
gileanu <gileanu@gmail.com>
2024-02-20 14:50:47 +0200

    [feat] added product card todo: fix price formatting, fix line 40 data.category?.name not working

---
+++ components/ui/product-card.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { Product } from "@/types";
+import Image from "next/image";
+import IconButton from "@/components/ui/icon-button";
+import Currency from "@/components/ui/currency";
+import { Expand, ShoppingCart } from "lucide-react";
+
+interface ProductCard {
+  data: Product;
+}
+
+const ProductCard: React.FC<ProductCard> = ({ data }) => {
+  return (
+    <div className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4">
+      <div className="aspect-square rounded-xl bg-grey-100 relative">
+        <Image
+          src={data?.images?.[0]?.url}
+          fill
+          alt="Product Image"
+          className="aspect-square object-cover rounded-md"
+        />
+        <div className="opacity-0 group-hover:opacity-100 transition absolute w-full px-6 bottom-5">
+          <div className="flex gap-x-6 justify-center">
+            <IconButton
+              onClick={() => {}}
+              icon={<Expand size={20} className="text-grey-500" />}
+            />
+            <IconButton
+              onClick={() => {}}
+              icon={<ShoppingCart size={20} className="text-grey-500" />}
+            />
+          </div>
+        </div>
+      </div>
+      {/*todo: add description*/}
+      <div>
+        <p className="font-semibold text-lg">{data.name}</p>
+        {/** fix Property 'name' does not exist on type 'string' */}
+        <p className="text-sm text-grey-500">{data.category?.name}</p>
+      </div>
+      {/*todo: price*/}
+      <div className="flex items-center justify-between">
+        <Currency value={data?.price} />
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
