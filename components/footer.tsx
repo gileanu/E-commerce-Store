00f9c0485fa365656544aac0e16d01cb21f5bfe4
gileanu <gileanu@gmail.com>
2024-02-20 16:23:11 +0200

    [style] updated copyright info, github link

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,7 +3,10 @@ const Footer = () => {
     <footer className="bg-white border-t">
       <div className="mx-auto py-10">
         <p className="text-center test-xs text-black">
-          &copy; 2024 gileanu, All rights reserved.
+          &copy; 2024 [name], [rights] - [terms] [conditions].
+        </p>
+        <p className="text-center text-xs text-gray-500">
+          RR - <a href="https://github.com/gileanu">github.com/gileanu</a>
         </p>
       </div>
     </footer>
