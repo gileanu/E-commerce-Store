0e635ff3d35e69782925b806bf2d068616f76d9c
gileanu <gileanu@gmail.com>
2024-02-21 13:55:31 +0200

    [feat] Footer Year now updating automatically

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,12 +1,16 @@
+import React from "react";
+
+const currnetYear = new Date().getFullYear();
+
 const Footer = () => {
   return (
     <footer className="bg-white border-t">
       <div className="mx-auto py-10">
         <p className="text-center test-xs text-black">
-          &copy; 2024 [name], [rights] - [terms] [conditions].
+          &copy; {currnetYear} [name], [rights] - [terms] [conditions].
         </p>
         <p className="text-center text-xs text-gray-500">
-          RR - <a href="https://github.com/gileanu">github.com/gileanu</a>
+          <a href="https://github.com/gileanu">RR - github.com/gileanu</a>
         </p>
       </div>
     </footer>
